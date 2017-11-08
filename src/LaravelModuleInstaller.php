71692a3a59a88a42234b9033f961ee3f665c69f0
Josh Brown <josh@kalexiko.com>
2017-11-08 10:06:12 +0000

    Refactoring and amendments to #4

--- src/LaravelModuleInstaller.php
+++ src/LaravelModuleInstaller.php
@@ -12,48 +12,66 @@ class LaravelModuleInstaller extends LibraryInstaller
     const DEFAULT_ROOT = "Modules";
 
     /**
+     * Get the fully-qualified install path
      * {@inheritDoc}
      */
     public function getInstallPath(PackageInterface $package)
     {
-        return $this->getBasePath() . '/' . $this->getModuleName($package);
+        return $this->getBaseInstallationPath() . '/' . $this->getModuleName($package);
     }
 
-    protected function getBasePath()
+    /**
+     * Get the base path that the module should be installed into.
+     * Defaults to Modules/ and can be overridden in the module's composer.json.
+     * @return string
+     */
+    protected function getBaseInstallationPath()
     {
-        if (! $this->composer || ! $this->composer->getPackage())
+        if (!$this->composer || !$this->composer->getPackage()) {
             return self::DEFAULT_ROOT;
+        }
 
         $extra = $this->composer->getPackage()->getExtra();
-        if (! $extra || empty($extra['module-dir']))
+
+        if (!$extra || empty($extra['module-dir'])) {
             return self::DEFAULT_ROOT;
+        }
 
         return $extra['module-dir'];
     }
 
+    /**
+     * Get the module name, i.e. "joshbrw/something-module" will be transformed into "Something"
+     * @param PackageInterface $package
+     * @return string
+     * @throws \Exception
+     */
     protected function getModuleName(PackageInterface $package)
     {
         $name = $package->getPrettyName();
         $split = explode("/", $name);
 
-        if (count($split) !== 2)
+        if (count($split) !== 2) {
             throw new \Exception($this->usage());
+        }
 
         $splitNameToUse = explode("-", $split[1]);
 
-        if (count($splitNameToUse) < 2)
+        if (count($splitNameToUse) < 2) {
             throw new \Exception($this->usage());
+        }
 
-        if (array_pop($splitNameToUse) !== 'module')
+        if (array_pop($splitNameToUse) !== 'module') {
             throw new \Exception($this->usage());
+        }
 
-        $final = '';
-        foreach($splitNameToUse as $part)
-            $final .= ucfirst($part);
-
-        return $final;
+        return implode('',array_map('ucfirst', $splitNameToUse));
     }
 
+    /**
+     * Get the usage instructions
+     * @return string
+     */
     protected function usage()
     {
         return "Ensure your package's name is in the format <vendor>/<name>-<module>";
