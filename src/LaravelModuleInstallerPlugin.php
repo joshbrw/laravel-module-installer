d397f4958c6121513254f1f5d700f9497147b7f5
Josh Brown <josh@joshbrown.me>
2020-10-30 18:17:43 +0000

    Implement plugin interface methods

--- src/LaravelModuleInstallerPlugin.php
+++ src/LaravelModuleInstallerPlugin.php
@@ -8,11 +8,21 @@ use Composer\Plugin\PluginInterface;
 
 class LaravelModuleInstallerPlugin implements PluginInterface
 {
-
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new LaravelModuleInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $installer = new LaravelModuleInstaller($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        $installer = new LaravelModuleInstaller($io, $composer);
+        $composer->getInstallationManager()->removeInstaller($installer);
+    }
 }
